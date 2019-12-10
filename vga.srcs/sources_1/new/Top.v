2cb5f28529c7644e39315dfdff4e25dc8beaa498
Mushatt <jemushatt@wpi.edu>
2019-12-10 16:09:49 -0500

    Added new controller files

---
+++ vga.srcs/sources_1/new/Top.v
@@ -0,0 +1,97 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/10/2019 02:57:30 PM
+// Design Name: 
+// Module Name: Top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Top(
+    // 50MHz clock input
+    input clk,
+    // Input from reset button (active low)
+    input rst_n,
+    // cclk input from AVR, high when AVR is ready
+    input cclk,
+   
+  
+
+    // VGA connections
+    output reg [2:0] pixelR,
+    output reg [2:0] pixelG,
+    output reg [2:0] pixelB,
+    output hsync_out,
+    output vsync_out
+    );
+
+wire clk_25;
+wire rst = rst_n; // make reset active high
+wire inDisplayArea;
+wire [9:0] CounterX;
+wire [8:0] CounterY;
+wire [3:0] selector;
+
+clk_25MHz clk_video(
+  .CLK_IN1(clk),
+  .CLK_OUT1(clk_25),
+  .RESET(rst)
+);
+
+hvsync_generator hvsync(
+  .clk(clk_25),
+  .vga_h_sync(hsync_out),
+  .vga_v_sync(vsync_out),
+  .CounterX(CounterX),
+  .CounterY(CounterY),
+  .inDisplayArea(inDisplayArea)
+);
+
+
+
+
+
+// use this bits from CounterX to divide the
+// horizontal screen in strips 64 bytes wide
+assign selector = CounterX[9:6];
+
+always @(posedge clk_25)
+begin
+  if (inDisplayArea) begin
+      /*
+       * I don't know if there is a smarter way to assign
+       * only one color for strip.
+       *
+       * Maybe using the selector signal as index?
+       */
+      pixelR[2] <= selector == 4'b0000;
+      pixelR[1] <= selector == 4'b0001;
+      pixelR[0] <= selector == 4'b0010;
+      pixelG[2] <= selector == 4'b0011;
+      pixelG[1] <= selector == 4'b0100;
+      pixelG[0] <= selector == 4'b0101;
+      pixelB[2] <= selector == 4'b0110;
+      pixelB[1] <= selector == 4'b0111;
+      pixelB[0] <= selector == 4'b1000;
+  end
+  else begin// if it's not to display, go dark
+      pixelR <= 3'b000;
+      pixelG <= 3'b000;
+      pixelB <= 3'b000;
+  end
+end
+
+endmodule
---
+++ vga.srcs/sources_1/new/hvsync_generator_1680x1050.v
@@ -0,0 +1,89 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/10/2019 04:02:25 PM
+// Design Name: 
+// Module Name: hvsync_generator_1680x1050
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/10/2019 02:56:10 PM
+// Design Name: 
+// Module Name: hvsync_generator
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module hvsync_generator_1680x1050(
+    input clk,
+    output vga_h_sync,
+    output vga_v_sync,
+    output reg inDisplayArea,
+    output reg [9:0] CounterX,
+    output reg [8:0] CounterY
+  );
+    reg vga_HS, vga_VS;
+
+    wire CounterXmaxed = (CounterX == 800); // 16 + 48 + 96 + 640
+    wire CounterYmaxed = (CounterY == 525); // 10 + 2 + 33 + 480
+
+    always @(posedge clk)
+    if (CounterXmaxed)
+      CounterX <= 0;
+    else
+      CounterX <= CounterX + 1;
+
+    always @(posedge clk)
+    begin
+      if (CounterXmaxed)
+      begin
+        if(CounterYmaxed)
+          CounterY <= 0;
+        else
+          CounterY <= CounterY + 1;
+      end
+    end
+
+    always @(posedge clk)
+    begin
+      vga_HS <= (CounterX > (640 + 16) && (CounterX < (640 + 16 + 96)));   // active for 96 clocks
+      vga_VS <= (CounterY > (480 + 10) && (CounterY < (480 + 10 + 2)));   // active for 2 clocks
+    end
+
+    always @(posedge clk)
+    begin
+        inDisplayArea <= (CounterX < 640) && (CounterY < 480);
+    end
+
+    assign vga_h_sync = ~vga_HS;
+    assign vga_v_sync = ~vga_VS;
+
+endmodule
